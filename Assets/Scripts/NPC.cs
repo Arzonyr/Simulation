305e2052d8b09a31aff21829cb7e614dae5f28b8
WaterFlower\Lasse Diedrich <lasse-d@freenet.de>
2018-06-27 20:02:50 +0200

    Added town level

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -19,12 +19,10 @@ public class NPC : MonoBehaviour
         wayPoints = GameObject.FindGameObjectsWithTag("WayPoint");
         for (int i = 0; i < points.Length; i++)
         {
-            points[i] = wayPoints[i].transform;
+            points[i] = wayPoints[Random.Range(0,7)].transform;
         }
      
-        // Disabling auto-braking allows for continuous movement
-        // between points (ie, the agent doesn't slow down as it
-        // approaches a destination point).
+        
         agent.autoBraking = false;
 
         GotoNextPoint();
@@ -33,23 +31,22 @@ public class NPC : MonoBehaviour
 
     void GotoNextPoint()
     {
-        // Returns if no points have been set up
         if (points.Length == 0)
             return;
 
-        // Set the agent to go to the currently selected destination.
+       
         agent.destination = points[destPoint].position;
 
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
+      
+      
         destPoint = (destPoint + 1) % points.Length;
     }
 
 
     void Update()
     {
-        // Choose the next destination point when the agent gets
-        // close to the current one.
+        
+       
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
             GotoNextPoint();
     }
